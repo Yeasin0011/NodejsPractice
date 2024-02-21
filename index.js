47b319942eb8aabfc00b68efcda51f6144f4d7ed
Yeasin0011 <yeasin.ibne.kadir@g.bracu.ac.bd>
2024-02-21 22:29:02 +0600

    01

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("hello")
+
+const x0=require('./practice')
+
+console.log(x0.addition(10,20)); 
+console.log(x0.subs(20,10))
---
+++ practice.js
@@ -0,0 +1,14 @@
+const addition = (x, y) => {
+    return (x+y)
+}
+
+const subs = (x, y)=> {
+    return (x-y)
+}
+
+// exports.add=addition;
+
+module.exports={
+    addition,
+    subs
+}
